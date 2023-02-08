177486f4124458f26a57e70334d5ef6ae9c916a6
pondkarun <pondkarun@gmail.com>
2023-02-08 11:41:11 +0700

    antd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import { Avatar, Dropdown, Select } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
-  const onClick = () => {
-    console.log('hello :>> TTT');
+  const onChange = (value) => {
+    console.log('hello :>> TTT', value);
   }
   return (
     <div className='main'>
-      <div className="nav" onClick={onClick}>
-        Allkons Family
+      <div className="nav">
+        <div className="pr-10">
+          <Select
+            size="small"
+            defaultValue="C001"
+            onChange={onChange}
+            style={{ width: 120 }}
+            options={[
+              { value: 'C001', label: 'C001' },
+              { value: 'C002', label: 'C002' },
+              { value: 'C003', label: 'C003' },
+              { value: 'C004', label: 'C004' },
+            ]}
+          />
+          <span className="pr-10" />
+          <span className="pr-10">นายแอดมิน เป็นคนดี</span>
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: '1',
+                  label: (
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                      บัญชีของฉัน
+                    </a>
+                  ),
+                },
+                {
+                  key: '2',
+                  label: (
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                      ออกจากระบบ
+                    </a>
+                  ),
+                },
+              ],
+            }}
+            placement="bottomLeft"
+            arrow
+          >
+            <Avatar size="small" />
+          </Dropdown>
+
+        </div>
       </div>
       <div className='nav-line' />
     </div>
